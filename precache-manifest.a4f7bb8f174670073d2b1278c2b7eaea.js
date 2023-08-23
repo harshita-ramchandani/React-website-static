d50e42e890a597a1fdacdf74ce5043fdfa2e6f18
Harshita Ramchandani <harshita.ramchandani@iitgn.ac.in>
2023-08-24 00:45:33 +0530

    Updates

---
+++ precache-manifest.a4f7bb8f174670073d2b1278c2b7eaea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab7d03cdb365b2c8e525",
+    "url": "/React-website-static/static/css/main.b40bd14c.chunk.css"
+  },
+  {
+    "revision": "ab7d03cdb365b2c8e525",
+    "url": "/React-website-static/static/js/main.5ea4396c.chunk.js"
+  },
+  {
+    "revision": "d248af2799fdc0342547",
+    "url": "/React-website-static/static/js/runtime~main.89ac88e5.js"
+  },
+  {
+    "revision": "bbe084307d3a604fdf51",
+    "url": "/React-website-static/static/js/2.ea2d579f.chunk.js"
+  },
+  {
+    "revision": "9f5fd5f3c476bb86b433",
+    "url": "/React-website-static/static/js/3.6ccac940.chunk.js"
+  },
+  {
+    "revision": "d85cec468342f3dcee087c02bf6dd430",
+    "url": "/React-website-static/static/media/pizzaLogo.d85cec46.png"
+  },
+  {
+    "revision": "4aa5292cac69561b68b10b2db0f3f6c1",
+    "url": "/React-website-static/static/media/pizza.4aa5292c.jpeg"
+  },
+  {
+    "revision": "08e289f85265b3d2b3984db3d424b705",
+    "url": "/React-website-static/static/media/pepperoni.08e289f8.jpg"
+  },
+  {
+    "revision": "434161f0b4c193f9ce5e1061f26dbf40",
+    "url": "/React-website-static/static/media/margherita.434161f0.jpg"
+  },
+  {
+    "revision": "05f53a063fe35296c578593c523ab1a1",
+    "url": "/React-website-static/static/media/pedrotechspecial.05f53a06.jpg"
+  },
+  {
+    "revision": "1672e7ad52375097a95ec88034fbc0cb",
+    "url": "/React-website-static/static/media/vegan.1672e7ad.jpg"
+  },
+  {
+    "revision": "44f59bed20e652447ce9f2f2c9340c65",
+    "url": "/React-website-static/static/media/pineapple.44f59bed.jpg"
+  },
+  {
+    "revision": "7f204ab1930eb1878e1a3326ec315244",
+    "url": "/React-website-static/static/media/expensive.7f204ab1.jpg"
+  },
+  {
+    "revision": "67f4f9031213dbbb6c7aaf1795c57ef8",
+    "url": "/React-website-static/static/media/multiplePizzas.67f4f903.jpeg"
+  },
+  {
+    "revision": "e29e930c1870f3fcc15edb3a8c3ade19",
+    "url": "/React-website-static/static/media/pizzaLeft.e29e930c.jpg"
+  },
+  {
+    "revision": "1f9f08871a7d5d43116ce919bea5b598",
+    "url": "/React-website-static/index.html"
+  }
+];
